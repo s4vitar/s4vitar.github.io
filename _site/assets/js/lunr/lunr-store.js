f76f66ed8b35dac0fff08ff43ae59186ff03e2f8
s4vitar <s4vitar@localhost.localdomain>
2020-01-30 11:04:24 +0000

    Segundo artículo - bspwm

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"Durante los últimos meses, hemos visto noticias por todos lados de datos médicos de pacientes los cuales han quedado expuestos por internet. Es algo alarmante pero no sorprendente teniendo en cuenta cómo está la seguridad a día de hoy. En el siguiente artículo, os explico en qué consiste este “gran...","categories": ["research"],
         "tags": ["hospitales","dicom","radiant"],
         "url": "http://localhost:4000/hospital-dicom-exposure/",
-        "teaser":"http://localhost:4000/assets/images/hospital-dicom/dicom.png"}]
+        "teaser":"http://localhost:4000/assets/images/hospital-dicom/dicom.png"},{
+        "title": "Archivos de configuración de Bspwm",
+        "excerpt":"Para el que no sepa de lo que estamos hablando, toda la configuración que se define en este artículo es la detallada en el siguiente vídeo mi canal de YouTube: Cómo configurar un buen entorno de trabajo en Linux Algunos de vosotros me habéis contactado pidiéndome que subiera los archivos...","categories": ["YouTube","Utilidades","Configuración"],
+        "tags": ["bspwm","sxhkd","compton","polybar","powerlevel10k"],
+        "url": "http://localhost:4000/bspwm-configuration-files/",
+        "teaser":"http://localhost:4000/assets/images/bspwm/bspwm-background.jpg"}]
